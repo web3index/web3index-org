7d742e5c12f238f554b9d993fb1e2b0df80f374c
adamsoffer <adam@livepeer.org>
2021-04-16 10:45:00 -0400

    update seo description

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,11 +1,11 @@
 export default {
   title: "The Web3 Index",
   description:
-    "The Web3 Index tracks key usage metrics for protocols across the entire Web3 stack so you can stay up to date on the latest Web3 trends",
+    "The Web3 Index tracks key revenue metrics for protocols across the Web3 stack so you can stay up to date on the latest Web3 trends.",
   openGraph: {
     title: "The Web3 Index",
     description:
-      "The Web3 Index tracks key usage metrics for protocols across the entire Web3 stack so you can stay up to date on the latest Web3 trends",
+      "The Web3 Index tracks key revenue metrics for protocols across the Web3 stack so you can stay up to date on the latest Web3 trends.",
     type: "website",
     locale: "en_IE",
     url: "https://web3index.org",
