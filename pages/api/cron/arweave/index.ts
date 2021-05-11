8995df2ac9e812f4c3990842bb75f757bcc6190a
Adam Soffer <adam@livepeer.org>
2021-05-11 09:25:49 -0400

    Update index.ts

--- pages/api/cron/arweave/index.ts
+++ pages/api/cron/arweave/index.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-// TODO: update daily fee data
+// TODO: update Arweave daily revenue data
 // a cron job should hit this endpoint every half hour or so (can use github actions for cron)
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
   // Use the updatedAt field in the Day model and compare it with the
