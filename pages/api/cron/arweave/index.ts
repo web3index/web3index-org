744affd6d420a82cdd819d2def61418af1c95352
adamsoffer <adam@livepeer.org>
2021-05-11 02:03:27 -0400

    resolve api endpoint

--- pages/api/cron/arweave/index.ts
+++ pages/api/cron/arweave/index.ts
@@ -6,5 +6,5 @@ export default async (_req: NextApiRequest, res: NextApiResponse) => {
   // Use the updatedAt field in the Day model and compare it with the
   // timestamp associated with the fee, if it's less than the timestamp
   // then update the day's revenue
-  res.status(200);
+  res.status(200).json({});
 };
