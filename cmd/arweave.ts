15670740e857b11ecaca3fba44b0128920e38432
balda <marcotorello@gmail.com>
2021-07-09 22:32:41 +0200

    repo cleanup

--- cmd/arweave.ts
+++ cmd/arweave.ts
@@ -92,7 +92,6 @@ const arweaveImport = async () => {
   } catch (e) {
     throw new Error("unable to get last block id from blockchain.");
   }
-  lastBlockId = parsedId + 10000;
   console.log("last block id: " + lastBlockId);
 
   // Get block height for last imported id
